126e4bf244b97531643ff7e4f07f667f2d109a1b
Habibur <hraasif02@gmail.com>
2025-07-03 23:08:17 +0600

    nothing bro

---
+++ 28_Find_Index_of_First_Occurrence_in_String.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string haystack = "sadbutsad";
+    string needle = "sad";
+    int p=needle.size();
+    int q=haystack.size();
+    // for(int i=0;i<len())
+    cout<<p<<" "<<q;
+    
+
+    return 0;
+}
