c2efec33970b461ab9900688f4c69e31efc0837a
Habibur <hraasif02@gmail.com>
2025-07-03 23:22:23 +0600

    miltese na vai

--- 28_Find_Index_of_First_Occurrence_in_String.cpp
+++ 28_Find_Index_of_First_Occurrence_in_String.cpp
@@ -8,7 +8,35 @@ int main() {
     int q=haystack.size();
     // for(int i=0;i<len())
     cout<<p<<" "<<q;
-    
+    bool ok=false;
+
+    for (int i=0;i<q;i++)
+    {
+        if(needle[0]==haystack[i] && i+p-1>=q)
+        {
+            bool boom=true;
+            int l=i+1;
+            for(int k=1;k<p;k++)
+            {
+                if(needle[k]!=haystack[l]) boom=false;
+                else l++;
+            }
+            if (boom==true)
+            {
+                ok=true;
+
+            }
+        }
+
+
+        if(ok)
+        {
+            break;
+        }
+    }
+
+
+    cout<<ok;
 
     return 0;
 }
