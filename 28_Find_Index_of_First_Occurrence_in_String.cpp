e94a4653fb6a6ea8b9a8117204801192c24f6edd
Habibur <hraasif02@gmail.com>
2025-07-03 23:32:51 +0600

    Mile Gese Vai

--- 28_Find_Index_of_First_Occurrence_in_String.cpp
+++ 28_Find_Index_of_First_Occurrence_in_String.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    string haystack = "sadbutsad";
+    string haystack = "aadbutsad";
     string needle = "sad";
     int p=needle.size();
     int q=haystack.size();
     // for(int i=0;i<len())
     cout<<p<<" "<<q;
     bool ok=false;
-
+    int x=-1;
     for (int i=0;i<q;i++)
     {
-        if(needle[0]==haystack[i] && i+p-1>=q)
+        if(needle[0]==haystack[i] && i+p-1<=q)
         {
             bool boom=true;
             int l=i+1;
@@ -31,12 +31,13 @@ int main() {
 
         if(ok)
         {
+            x=i;
             break;
         }
     }
 
 
-    cout<<ok;
+    cout<<"Bro Found"<<x<<'\n';
 
     return 0;
 }
