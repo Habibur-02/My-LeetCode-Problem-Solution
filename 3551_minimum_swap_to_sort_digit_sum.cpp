e4702c84135acd1c91e7bd4a50ce66dee588e667
Habibur <hraasif02@gmail.com>
2025-07-04 22:36:30 +0600

    not as leetcode format

---
+++ 3551_minimum_swap_to_sort_digit_sum.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Helper function to calculate sum of digits
+    int f(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    // Main function to calculate minimum swaps
+    int minSwaps(vector<int>& nums) {
+        vector<pair<pair<int, int>, int>> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            int ele = f(nums[i]);
+            ans.push_back({{ele, nums[i]}, i});
+        }
+
+        // Sort based on sum of digits first, then the number itself
+        sort(ans.begin(), ans.end());
+
+        int n = nums.size();
+        int res = 0;
+        vector<bool> vis(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i] || ans[i].second == i) continue;
+            int c = 0;
+            int j = i;
+            while (!vis[j]) {
+                vis[j] = true;
+                j = ans[j].second;
+                c++;
+            }
+            if (c > 1) res += c - 1;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {21, 12, 33, 14};  // Example input
+    cout << "Minimum swaps required: " << obj.minSwaps(nums) << endl;
+    return 0;
+}
