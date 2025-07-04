3cb8536f337e0d61c7107b0951ca5e328a9e5d47
Habibur <hraasif02@gmail.com>
2025-07-05 00:16:38 +0600

    vector<pair>

--- recap_vector.cpp
+++ recap_vector.cpp
@@ -6,29 +6,43 @@ int main() {
     // Creating an empty vector
     vector<int>v;
 
-    int n;
-    cin>>n;
+    // int n;
+    // cin>>n;
 
-    for (int i=0;i<n;i++)
-    {
-        int x;
-        cin>>x;
-        v.push_back(x);
-    }
+    // for (int i=0;i<2;i++)
+    // {
+    //     int x;
+    //     cin>>x;
+    //     v.push_back(x);
+    // }
     // sort(v.begin(),v.end());
-    auto it=find(v.begin(),v.end(),9);
+    // auto it=find(v.begin(),v.end(),9);
     // cout<<it;
     // for(int i=0;i<v.size();i++)
     // cout<<v[i]<<" ";
-    if(it==v.end()) cout<<"Not Found"<<'\n';
-    else cout<<distance(v.begin(),it)<<'\n'; //it will print first occurance
-    cout<<distance(v.begin(),it);
+    // if(it==v.end()) cout<<"Not Found"<<'\n';
+    // else cout<<distance(v.begin(),it)<<'\n'; //it will print first occurance
+    // cout<<distance(v.begin(),it)<<'\n';
+
+    vector<pair<int,int>>v1;
+    for(int i=0;i<5;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        v1.push_back({a,b});
+    }
+    for (auto k:v1) cout<<k.first<<" "<<k.second<<" ";
+    cout<<'\n';
+    sort(v1.begin(),v1.end());
+
+    for(auto l:v1) cout<<l.first<<" "<<l.second<<" ";
+
+    // for (int i=0;i<5;i++)
+    // {
 
 
-    
+    // }
 
-
-    
     return 0;
 
 }
