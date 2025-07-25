2e4b3e957dcfe7e933b61d397da0bb683c9b608e
Habibur <hraasif02@gmail.com>
2025-07-25 23:11:46 +0600

    sliding window

---
+++ min_subarray_sum.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    vector<int>v={1,4,4};
+    int target=4;
+    int ss=0;
+
+    for (auto l:v) ss+=l;
+    if(ss<target) cout<<0;
+    if(ss==target) cout<<v.size();
+
+    int s=0;
+    int e=0;
+    int minm=INT_MAX;
+    int sum=0;
+    while (s<v.size())
+    {
+        /* code */
+        sum+=v[s];
+
+        if(sum>=target)
+        {
+            while(e<=s && sum>=target)
+            {
+            minm=min(minm,s-e+1);
+            cout<<"loop no- "<<s<<" "<<e<<" ";
+            cout<<minm<<'\n';
+            sum-=v[e];
+            e++;
+            }
+            
+        }
+        s++;
+
+
+    }
+    
+   cout<<minm;
+
+}
