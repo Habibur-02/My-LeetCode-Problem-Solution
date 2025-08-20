d0143332a61ea69bc0140fc02e602a9127a2605a
Habibur <hraasif02@gmail.com>
2025-08-20 22:37:37 +0600

    failed

--- divide_and_conquar_practice.cpp
+++ divide_and_conquar_practice.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 int divideConquar(string s, int start, int end, int k)
 {
- if (s.size()<k) return 0;
+ if (s.empty()) return 0;
  int countmap[26]={0};
- for (int i=0;i<26;i++) countmap[s[i]-'a']++;
- for(int i=0;i<26;i++) cout<<countmap[i]<<" ";
- 
+//  for (int i=0;i<26;i++) countmap[s[i]-'a']++;
+//  for(int i=0;i<26;i++) cout<<countmap[i]<<" ";
+int mid=start+end;
+mid=mid/2;
+
+
+
+cout<<"String Information"<<" "<<s<<" start "<<start<<" end "<<end<<'\n';
+string a=s.substr(start, mid);
+string b=s.substr(mid, end);
+
+return divideConquar(a, start, mid, k) + divideConquar(b, mid,end, k);
+
+
 
 
 
@@ -15,8 +26,9 @@ int main()
 {
 
     string s="aaaabbvavavavvbbbbbabbab";
-    int k=2;
+    int k=1;
     int n=s.size();
-    int ans=divideConquar(s,0,n,k);
+    // int ans=divideConquar(s,0,n,k);
+    cout<<s.substr(20,24);
 
 }
