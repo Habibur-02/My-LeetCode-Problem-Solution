5e997d7855ffc0ba1f3c6689ecb109ff3cf58edc
Habibur <hraasif02@gmail.com>
2025-08-20 22:19:17 +0600

    trying

--- divide_and_conquar_practice.cpp
+++ divide_and_conquar_practice.cpp
@@ -4,7 +4,10 @@ int divideConquar(string s, int start, int end, int k)
 {
  if (s.size()<k) return 0;
  int countmap[26]={0};
+ for (int i=0;i<26;i++) countmap[s[i]-'a']++;
+ for(int i=0;i<26;i++) cout<<countmap[i]<<" ";
  
+
 
 
 }
