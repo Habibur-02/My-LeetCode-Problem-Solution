f12a4312c7dcb33cbb0cc0e5e1ec90aba83b2dc8
Habibur <hraasif02@gmail.com>
2025-08-20 22:17:25 +0600

    trying to do divide and conquaer algo

---
+++ divide_and_conquar_practice.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int divideConquar(string s, int start, int end, int k)
+{
+ if (s.size()<k) return 0;
+ int countmap[26]={0};
+ 
+
+
+}
+int main()
+{
+
+    string s="aaaabbvavavavvbbbbbabbab";
+    int k=2;
+    int n=s.size();
+    int ans=divideConquar(s,0,n,k);
+
+}
