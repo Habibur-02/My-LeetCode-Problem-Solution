9e2060d5b80ed9300b665f0d2a6416a064b12f77
Habibur <hraasif02@gmail.com>
2025-08-21 15:38:04 +0600

    search_a element

--- search_an_element.cpp
+++ search_an_element.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int divide(int arr[], int start, int end, int i)
 {
 
---
+++ search_an_elements.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int divide(vector<int> & v, int start, int end, int k)
+{
+    if(start==end)
+    {
+        if(v[start]==k)
+        return true;
+        else return false;
+
+    }
+    int mid=start + end;
+    mid/=2;
+    return (divide(v,start, mid, k) + divide(v, mid+1,end, k));
+    // return false;
+}
+int main()
+{
+
+    vector<int>v={10,1,5,2,8,3,7,4,6,5};
+    int k=5;
+    int n=v.size();
+    cout<<divide(v,0,n-1, k);
+
+    // cout<<n;
+}
